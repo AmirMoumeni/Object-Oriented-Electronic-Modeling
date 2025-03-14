397973323b870b57965e1c74608d77eca7a71024
Amir <amirmoumeni83@gmail.com>
2025-03-14 23:06:59 +0330

    Upload

--- CA2/src/main.cpp
+++ CA2/src/main.cpp
@@ -13,16 +13,20 @@ int main() {
     Wire *out = new Wire();
 
     a->setValue(ZERO);
+    cout << "00 : " << a->value() <<endl;
     b->setValue(ONE);
+    cout << "01 : " << b->value() <<endl;
     c->setValue(ONE);
+    cout << "10 : " << c->value() <<endl;
     d->setValue(ZERO);
+    cout << "11 : " << d->value() <<endl;
     s1->setValue(ZERO);
-    s2->setValue(ONE);
-
+    s2->setValue(ZERO);
+    cout << "Sel : " << s1->value() << s2->value() <<endl;
     Mux* my_mux = new Mux(*s1, *s2, *a, *b, *c, *d, *out, 0);
     my_mux->evl();
 
-    cout << "output : " << out->value();
+    cout << "output : " << out->value() << endl;
 
     return 0;
 }
